ec77aa908d891f2c6cbe36df48c82756708294ee
Waseemkhan950 <waseemadil950@gmail.com>
2025-03-06 01:22:52 +0500

    completed the static part of Worldwise app

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Homepage from "./pages/Homepage";
+import Pricing from "./pages/Pricing";
+import Product from "./pages/Product";
+import PageNotFound from "./pages/PageNotFound";
+import AppLayout from "./pages/AppLayout";
+import Login from "./pages/Login";
 function App() {
 	return (
 		<>
-			<h1>hello React</h1>
+			<BrowserRouter>
+				<Routes>
+					<Route path="/" element={<Homepage />} />
+					<Route path="/pricing" element={<Pricing />} />
+					<Route path="/product" element={<Product />} />
+					{<Route path="/app" element={<AppLayout />} />}
+					{<Route path="/login" element={<Login />} />}
+					<Route path="*" element={<PageNotFound />} />
+				</Routes>
+			</BrowserRouter>
 		</>
 	);
 }
